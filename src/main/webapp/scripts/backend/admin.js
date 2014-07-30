4ea575ee824f41d383e1ac039071e9fafbcf863c
gongmingqm10 <=>
2014-07-30 09:56:03 +0800

    debug admin.jsp by xu

--- src/main/webapp/scripts/backend/admin.js
+++ src/main/webapp/scripts/backend/admin.js
@@ -1,9 +1,11 @@
 $(function () {
-    var path = window.location.pathname.split('/').pop();
+    var path = window.location.pathname.split('/');
     var children=$(document.getElementById("menu-ul")).children();
     for (var i in children) {
-        if(children[i].className === path){
-            $(children[i]).addClass("active");
+        for(var j in path){
+            if(children[i].className === path[j]){
+                $(children[i]).addClass("active");
+            }
         }
     }
 } );
